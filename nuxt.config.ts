1dc8e051f3c1295fde3985f73ea369bd3218b63a
Oleksandr <krux228@gmail.com>
2025-04-21 23:20:42 +0300

    1
    1

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,11 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   modules: ['@nuxtjs/tailwindcss', '@pinia/nuxt'],
+  
+  // Add runtime config
+  runtimeConfig: {
+    public: {
+      apiBase: process.env.NUXT_PUBLIC_API_BASE || '/api'
+    }
+  }
 })
